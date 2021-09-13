7c0997fb566f9e7d09c24c24d404f4221c8d76bc
jeonyuzin <junyouzin4209@naver.com>
2021-09-13 19:50:22 +0900

    version week3 0.2

--- syntax04.go
+++ syntax04.go
@@ -6,7 +6,7 @@ import (
 	"time" //seed 생성용 패키지
 )
 
-//난수 추출된 수의 소수 판정 프로그램
+//난수 추출된 수의 소수 판정 프로그램 ver0.2
 func main() {
 	seed := time.Now().Unix() //seed설정
 	rand.Seed(seed)
@@ -15,13 +15,13 @@ func main() {
 	number := rand.Intn(150) + 2 //2~151사이에수
 	fmt.Println("임의 로 추출된 수 ", number)
 
-	for i := 1; i <= number; i++ {
+	for i := 2; i < number; i++ {
 		if number%i == 0 {
 			count++
 		}
 
 	}
-	if count == 2 {
+	if count == 0 {
 		fmt.Println(number, "는 소수 입니다.")
 	} else {
 		fmt.Println(number, "는 소수가 아닙니다.")
